591a23b164bf47052c9fe8aaaa10692982c6a7d2
thebeen11 <bagus11nugroho@gmail.com>
2022-03-23 01:39:07 +0700

    create accordion

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -3,8 +3,8 @@ import {Container,Inner, Item, Pane, Title, SubTitle, Image} from './styles/jumb
 
 export default function Jumbotron({children, direction = 'row', ...restProps}) {
     return(
-        <Item direction={direction}>
-            <Inner>{children}</Inner>
+        <Item {...restProps}>
+            <Inner  direction={direction}>{children}</Inner>
         </Item>
     )
 }
---
+++ src/containers/faqs.js
@@ -0,0 +1,27 @@
+import faqsData from "../fixtures/faqs.json";
+import { Accordion, OptForm } from "../components";
+
+export function FaqsContainer({}) {
+  return (
+    <Accordion>
+      <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+      <Accordion.Item>
+        {faqsData.map((item) => (
+          <Accordion.Item key={item.id}>
+            <Accordion.Header>{item.header}</Accordion.Header>
+            <Accordion.Body>{item.body}</Accordion.Body>
+          </Accordion.Item>
+        ))}
+      </Accordion.Item>
+
+      <OptForm>
+        <OptForm.Input placeholder="Email Address" />
+        <OptForm.Button>Try it now</OptForm.Button>
+        <OptForm.Break/>
+        <OptForm.Text>
+          Ready to watch? Enter your email to create or restart your membership
+        </OptForm.Text>
+      </OptForm>
+    </Accordion>
+  );
+}
