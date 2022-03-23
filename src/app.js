c9354b8d46c874bc24ce80b31694997a0543043e
thebeen11 <bagus11nugroho@gmail.com>
2022-03-24 01:36:54 +0700

    react router and create header

--- src/app.js
+++ src/app.js
@@ -1,13 +1,24 @@
-import { FaqsContainer } from "./containers/faqs";
-import { FooterContainer } from "./containers/footer";
-import { JumbotronContainer } from "./containers/jumbotron";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import {Browse, Signin, Signup, Home} from './pages'
+import * as ROUTES from "./constans/routes";
 
 export default function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer/>
-      <FooterContainer />
-    </>
+    <Router>
+      <Route exact path='/browse'>
+        <Browse/>
+      </Route>
+      <Route exact path='/signin'>
+        <Signin/>
+      </Route>
+      <Route exact path='/signup'>
+        <Signup/>
+      </Route>
+      
+      <Route exact path={ROUTES.HOME}>
+        <Home/>
+      </Route>
+    </Router>
   );
 }
---
+++ src/components/header/index.js
@@ -0,0 +1,25 @@
+import { Link as ReactRouterLink } from 'react-router-dom'
+
+import {Background, Container, ButtonLink, Logo} from './styles/header'
+
+export default function Header({ bg = true, children, ...restProps }) {
+  return bg == true ? (
+    <Background {...restProps}>{children}</Background>
+  ) : (
+    children
+  );
+}
+
+Header.Frame = function HeaderFrame({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
+};
+
+Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
+  return <ButtonLink {...restProps}>{children}</ButtonLink>;
+};
+
+Header.Logo = function HeaderLogo({ to, ...restProps }) {
+  return (<ReactRouterLink to={to}>
+      <Logo {...restProps}/>
+  </ReactRouterLink>)
+};
