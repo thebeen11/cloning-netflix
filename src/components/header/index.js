243ca4807c36ee5fcb690aac93def89e5aa64e89
thebeen11 <bagus11nugroho@gmail.com>
2022-04-02 14:41:09 +0700

    search bar and play button

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,24 @@
-import { Link as ReactRouterLink } from 'react-router-dom'
+import { Link as ReactRouterLink } from "react-router-dom";
+import React, { useState } from 'react'
 
-import {Background, Container, ButtonLink, Logo} from './styles/header'
+import {
+  Background,
+  Container,
+  ButtonLink,
+  Logo,
+  Feature,
+  Text,
+  FeatureCallOut,
+  Link,
+  Group,
+  Picture,
+  Profile,
+  Dropdown,
+  Search,
+  SearchIcon,
+  SearchInput,
+  PlayButton
+} from "./styles/header";
 
 export default function Header({ bg = true, children, ...restProps }) {
   return bg == true ? (
@@ -10,16 +28,70 @@ export default function Header({ bg = true, children, ...restProps }) {
   );
 }
 
+Header.Feature = function HeaderFeature({ children, ...restProps }) {
+  return <Feature {...restProps}>{children}</Feature>;
+};
+
+
+Header.Profile = function HeaderProfile({ children, ...restProps }) {
+  return <Profile {...restProps}>{children}</Profile>;
+};
+
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({ children, ...restProps }) {
+  return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
+};
+
+Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
+  return <Dropdown {...restProps}>{children}</Dropdown>;
+};
+
+Header.Text = function HeaderText({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>;
+};
+
+Header.TextLink = function HeaderTextLink({ children, ...restProps }) {
+  return <Link {...restProps}>{children}</Link>;
+};
+
 Header.Frame = function HeaderFrame({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
+};
+
+
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+  return <Group {...restProps}>{children}</Group>;
 };
 
 Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
   return <ButtonLink {...restProps}>{children}</ButtonLink>;
 };
 
+Header.Picture = function HeaderPicture({ src, ...restProps }) {
+  return <Picture {...restProps} src={`/images/users/${src}.png`}/>
+};
+
+Header.Search = function HeaderSearch({ searchTerm, setsearchTerm, ...restProps }) {
+  const [searchActive, setsearchActive] = useState(false);
+
+  return(
+    <Search {...restProps}>
+      <SearchIcon onClick={()=> setsearchActive((searchActive) => !searchActive)}>
+        <img src="/images/icons/search.png" alt="search"/>
+      </SearchIcon>
+      <SearchInput value={searchTerm} onChange={({target})=>setsearchTerm(target.value)} placeholder="Search films and series" active={searchActive}/>
+    </Search>
+  )
+};
+
+Header.PlayButton = function HeaderPlayButton({ children, ...restProps }) {
+  return <PlayButton {...restProps}>{children}</PlayButton>;
+};
+
 Header.Logo = function HeaderLogo({ to, ...restProps }) {
-  return (<ReactRouterLink to={to}>
-      <Logo {...restProps}/>
-  </ReactRouterLink>)
+  return (
+    <ReactRouterLink to={to}>
+      <Logo {...restProps} />
+    </ReactRouterLink>
+  );
 };
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,9 +1,12 @@
 import React, { useContext, useState, useEffect } from "react";
 import { SelectProfileContainer } from "./profile";
 import { FirebaseContext } from "../context/firebase";
-import { Loading } from "../components";
+import { Header, Loading } from "../components";
+import * as ROUTES from "../constans/routes";
+import logo from "../logo.svg";
 
 export function BrowseContainer({ slides }) {
+  const [searchTerm, setsearchTerm] = useState('')
   const [profile, setprofile] = useState({});
   const [loading, setloading] = useState(true);
   const { firebase } = useContext(FirebaseContext);
@@ -16,9 +19,49 @@ export function BrowseContainer({ slides }) {
   }, [profile.displayName]);
 
   return profile.displayName ? (
-    loading ? (
-      <Loading src={user.photoURL} />
-    ) : <Loading.ReleaseBody/>
+    <>
+      {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+
+      <Header src="joker1" dontShowOnSmallViewPort>
+        <Header.Frame>
+          <Header.Group>
+            <Header.Logo to={ROUTES.HOME} alt="Netflix" src={logo} />
+            <Header.TextLink>Series</Header.TextLink>
+            <Header.TextLink>Film</Header.TextLink>
+          </Header.Group>
+          <Header.Group>
+            <Header.Search searchTerm={searchTerm} setsearchTerm={setsearchTerm} />
+            <Header.Profile>
+              <Header.Picture src={user.photoURL} />
+              <Header.Dropdown>
+                <Header.Group>
+                  <Header.Picture src={user.photoURL} />
+                  <Header.TextLink>{user.displayName}</Header.TextLink>
+                </Header.Group>
+                <Header.Group>
+                  <Header.TextLink onClick={() => firebase.auth().signOut()}>
+                    Sign out
+                  </Header.TextLink>
+                </Header.Group>
+              </Header.Dropdown>
+            </Header.Profile>
+          </Header.Group>
+        </Header.Frame>
+        <Header.Feature>
+          <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+          <Header.Text>
+            Forever alone in a crowd, failed comedian Arthur Fleck seeks
+            connection as he walks the streets of Gotham City. Arthur wears two
+            masks -- the one he paints for his day job as a clown, and the guise
+            he projects in a futile attempt to feel like he's part of the world
+            around him. Isolated, bullied and disregarded by society, Fleck
+            begins a slow descent into madness as he transforms into the
+            criminal mastermind known as the Joker.
+          </Header.Text>
+          <Header.PlayButton>Play</Header.PlayButton>
+        </Header.Feature>
+      </Header>
+    </>
   ) : (
     <SelectProfileContainer user={user} setprofile={setprofile} />
   );
